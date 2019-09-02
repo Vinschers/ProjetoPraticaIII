ae1243235c9c0b0f2d44064ab16feb19f1a43fdc
Scherer <fsvicentin@hotmail.com>
2019-09-02 11:12:11 -0300

    primeiro teste de código do controle arduino

--- controle/controle.ino
+++ controle/controle.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+#define del 150
+#define btnOk 2
+#define btnPot 3
+#define btnCima 4
+#define btnMenu 5
+#define btnBaixo 6
+#define btnDireita 7
+#define btnEsquerda 8
 
+String atual, anterior;
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(btnOk, INPUT);
+  pinMode(btnPot, INPUT);
+  pinMode(btnCima, INPUT);
+  pinMode(btnMenu, INPUT);
+  pinMode(btnBaixo, INPUT);
+  pinMode(btnDireita, INPUT);
+  pinMode(btnEsquerda, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  atual = digitalRead(btnOk) + "" + digitalRead(btnPot) + "" + digitalRead(btnCima) + "" + digitalRead(btnMenu) + "" + digitalRead(btnBaixo) + "" + digitalRead(btnDireita) + "" + digitalRead(btnEsquerda);
+  if (!anterior || atual != anterior) {
+    Serial.println(atual);
+    anterior = atual;
+  }
+  delay(del);
 }
