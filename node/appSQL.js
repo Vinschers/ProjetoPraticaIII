21adce36c325caf6293262d7e67fb49004b25897
Gustavo Miller <42343653+MillerGS03@users.noreply.github.com>
2019-11-04 11:13:52 -0300

    Node com banco de dados pronto

--- node/appSQL.js
+++ node/appSQL.js
@@ -53,6 +53,48 @@ const rota = express.Router();
 rota.get('/', (req, res) => {
   res.json(fases);
 });
+
+rota.post('/criarJogo', (req, res) => {
+  const slot = req.body.slot;
+  const ip = req.body.ip;
+  const tranquilidade = req.body.tranquilidade;
+  const felicidade = req.body.felicidade;
+  const sanidade = req.body.sanidade;
+  const financas = req.body.financas;
+  const inteligencia = req.body.inteligencia;
+  const carisma = req.body.carisma;
+  const forca = req.body.forca;
+
+  execSQL(`CriarJogo_sp ${slot}, '${ip}', ${tranquilidade}, ${felicidade}, ${sanidade}, ${financas}, ${inteligencia}, ${carisma}, ${forca}`, res);
+})
+rota.patch('/atualizarJogo', (req, res) => {
+  const id = req.body.id;
+  const slot = req.body.slot;
+  const acabouDeComecar = req.body.acabouDeComecar ? 1 : 0;
+  const tranquilidade = req.body.tranquilidade;
+  const felicidade = req.body.felicidade;
+  const sanidade = req.body.sanidade;
+  const financas = req.body.financas;
+  const inteligencia = req.body.inteligencia;
+  const carisma = req.body.carisma;
+  const forca = req.body.forca;
+  const caminho = req.body.caminho;
+  const nivelAtual = req.body.nivelAtual;
+  const linhaAtual = req.body.linhaAtual;
+
+  const amizades = req.body.amizades;
+
+  execSQLSemResposta(`update Jogo set slot=${slot}, acabouDeComecar=${acabouDeComecar}, tranquilidade=${tranquilidade}, felicidade=${felicidade}, sanidade=${sanidade}, financas=${financas}, inteligencia=${inteligencia}, carisma=${carisma}, forca=${forca}, caminho='${caminho}, nivelAtual='${nivelAtual}, linhaAtual='${linhaAtual}' where id=${id}`, res);
+  for (var i = 0; i < 6; i++)
+    execSQLSemResposta(`update JogoPersonagem set amizade=${amizades[i]} where idJogo=${id} and idPersonagem=${i}`, res);
+
+  res.json("Tudo certo :D");
+})
+rota.delete('/deletarJogo', (req, res) => {
+  const id = req.body.id;
+  execSQL(`DeletarJogo_sp ${id}`, res);
+})
+
 app.use('/', rota);
 
 //inicia servidor
@@ -63,5 +105,11 @@ function execSQL(sql, resposta) {
 	global.conexao.request()
 				  .query(sql)
 				  .then(resultado => resposta.json(resultado.recordset))
+				  .catch(erro => resposta.json(erro));
+}
+function execSQLSemResposta(sql, resposta) {
+	global.conexao.request()
+				  .query(sql)
+				  .then(resultado => resposta = resposta)
 				  .catch(erro => resposta.json(erro));
 }
