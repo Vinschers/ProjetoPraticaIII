a734e29ed2278650cb0c95cf5c115b19d40d2d9f
Gustavo Miller <42343653+MillerGS03@users.noreply.github.com>
2019-11-22 10:01:55 -0300

    Bug de início do jogo e controle com joystick e bluetooth

--- controle/controle.ino
+++ controle/controle.ino
@@ -1,99 +1,41 @@
 #define del 150
-#define btnOk 2
-#define btnCima 4
-#define btnMenu 5
-#define btnBaixo 6
-#define btnDireita 7
-#define btnCancelar 3
-#define btnEsquerda 8
+#define btnOk 2 
+#define btnMenu 5 
+#define btnCancelar 3 
+#define pinoX A0
+#define pinoY A1
 
+int pushButton = 2;
 String atual, anterior;
-#include "WiFiEsp.h"
-#include "SoftwareSerial.h"
-SoftwareSerial serial(8, 9);
-char ssid[] = "Scherer";
-char pass[] = "12345678";
-int status = WL_IDLE_STATUS;
-
-WiFiEspServer server(80);
-RingBuffer buf(8);
-
-
 void setup() {
-  Serial.begin(115200);  // porta de debug
-  serial.begin(9600);
-  while (!Serial) {
-    ;
-  }
-  WiFi.init(&serial);
-  // check for the presence of the shield
-  if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present");
-    while (true); // don't continue
-  }
-
-  Serial.print("Attempting to start AP ");
-  Serial.println(ssid);
-
-  // uncomment these two lines if you want to set the IP address of the AP
-  //IPAddress localIp(192, 168, 111, 111);
-  //WiFi.configAP(localIp);
-
-  // start access point
-  status = WiFi.beginAP(ssid, 10, pass, ENC_TYPE_WPA2_PSK);
-
-  Serial.println("Access point started");
-  //printWifiStatus();
-
-  // start the web server on port 80
-  server.begin();
-  Serial.println("Server started");
+  Serial.begin(9600);
+  Serial.println("ComunicaÃ§Ã£o iniciada!");
   pinMode(btnOk, INPUT);
-  pinMode(btnCima, INPUT);
   pinMode(btnMenu, INPUT);
-  pinMode(btnBaixo, INPUT);
-  pinMode(btnDireita, INPUT);
   pinMode(btnCancelar, INPUT);
-  pinMode(btnEsquerda, INPUT);
+
 }
 
 void loop() {
-  WiFiEspClient client = server.available();
-  if (client) {
-    Serial.println("Novo cliente");
-    buf.init();
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        buf.push(c);
-        Serial.write(c);
-        if (buf.endsWith("\r\n\r\n")) {
-          Serial.println("Fechando");
-          buf.reset();
-          break;
-        }
-        atual = lerBtns();
-        if (!anterior || atual != anterior) {
-          Serial.println(atual);
-          anterior = atual;
-          client.println(atual);
-        }
-        delay(del);
-      }
-    } // while
-    client.stop();
-    Serial.println("Desconectado");
-  }
+    atual = lerBtns();
+    if (!anterior || atual != anterior) {
+      Serial.println(atual);
+      anterior = atual;
+    }
+    delay(del);
 }
 
 String lerBtns() {
-  String ret = "";
-  ret += digitalRead(btnOk) + " ";
-  ret += digitalRead(btnCima) + " ";
-  ret += digitalRead(btnMenu) + " ";
-  ret += digitalRead(btnBaixo) + " ";
-  ret += digitalRead(btnDireita) + " ";
-  ret += digitalRead(btnCancelar) + " ";
-  ret += digitalRead(btnEsquerda);
+  int x = analogRead(pinoX);
+  int y = analogRead(pinoY);
+  
+  int esquerda = y > 800 ? 1 : 0,
+      direita  = y < 200 ? 1 : 0,
+      cima     = x < 200 ? 1 : 0,
+      baixo    = x > 800 ? 1 : 0;
+      
+  String ret = digitalRead(btnOk) + String(" ") + cima + " " + digitalRead(btnMenu) + " " + baixo + " " + direita + " " + digitalRead(btnCancelar) + " " + esquerda;
+  
   return ret;
 }
+
